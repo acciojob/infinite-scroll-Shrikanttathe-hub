0a2ceaa9f60dc4a2885f6b0cbc20ca6dc8cba227
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2026-01-28 12:40:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
+const listContainer =  document.querySelector('main');
+const list = document.getElementById('infi-list');
 
+let itemCount = 0;
+
+function addItems(count) {
+	for(let i=0; i<count; i++){
+		itemCount++;
+		const li = document.createElement('li');
+		li.textContent = `Item ${itemCount}`;
+		list.appendChild(li);
+	}
+}
+addItems(10);
+list.addEventListener('scroll', ()=> {
+	const scrollTop = list.scrollTop;
+	const scrollHeight = list.scrollHeight;
+	const clientHeight = list.clientHeight;
+
+	if(scrollTop + clientHeight >= scrollHeight - 5){
+		addItems(2);
+	}
+});
